8e805ab142565100c82efe45a9e9f2b497474e27
Farzad FARID <farzad.farid@kapten.com>
2020-05-04 19:28:57 +0200

    Refactor search with an iterator

--- src/lib.rs
+++ src/lib.rs
@@ -43,28 +43,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in content.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    content
+        .lines()
+        .filter(|&l| l.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
     let query = query.to_lowercase();
 
-    for line in content.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    content
+        .lines()
+        .filter(|&l| l.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
